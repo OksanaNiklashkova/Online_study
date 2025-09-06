ced42d5c08d1d08999920a4d70e916e6ca6a1ea9
Oksana Niklashkova <o.niklashkova@gmail.com>
2025-09-06 14:46:31 +0300

    add new fields for course

--- studies/serializers.py
+++ studies/serializers.py
@@ -3,15 +3,20 @@ from rest_framework import serializers
 from studies.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """сериалайзер для курса"""
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
-    """сериалайзер для урока"""
+    """сериализатор для урока"""
     class Meta:
         model = Lesson
         fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    """сериализатор для курса"""
+    lessons = LessonSerializer(many=True)
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ['title', 'preview', 'description', 'lessons_count', 'lessons']
